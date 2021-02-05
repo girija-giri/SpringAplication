1e99ad6ff6c9ca559398c625d9279edbd6b069e8
g.kolathur.viswanath <g.kolathur.viswanath@BDC7-L-B21BVN2.dir.svc.accenture.com>
2021-02-05 14:19:13 +0530

    main

--- src/main/java/com/example/restapi/DemoRestApi/PayrollApplication.java
+++ src/main/java/com/example/restapi/DemoRestApi/PayrollApplication.java
@@ -6,9 +6,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class PayrollApplication {
 
-  public static void main(String... args) {
+/*  public static void main(String... args) {
    // SpringApplication.run(PayrollApplication.class, args);
-  }
+  }*/
 }
 
 //http://localhost:8080/employees
