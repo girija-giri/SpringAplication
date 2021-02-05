29e01fafd96f38973393d7383043cd9038b0a1da
g.kolathur.viswanath <g.kolathur.viswanath@BDC7-L-B21BVN2.dir.svc.accenture.com>
2021-02-05 13:31:47 +0530

    initial commit

---
+++ src/main/java/com/demo/hello/DemoApplication.java
@@ -0,0 +1,14 @@
+package com.demo.hello;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import com.example.restapi.DemoRestApi.PayrollApplication;
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		  SpringApplication.run(DemoApplication.class, args);
+	}
+
+}
